83e269f5bd9b81f59e4efac69a5349ced8404e00
Tyson Hood <TysonHood@gmail.com>
2020-06-03 14:58:41 -0700

    Update our menu-items to navigate withRouter HOC

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
 
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:ital,wght@0,300;0,700;1,300&display=swap" rel="stylesheet"></link>
-  </React.StrictMode>,
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:ital,wght@0,300;0,700;1,300&display=swap" rel="stylesheet"></link>
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
