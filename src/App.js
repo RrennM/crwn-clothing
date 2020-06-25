5a18b7347e278f4b520b135517116598d8ba8317
Tyson Hood <TysonHood@gmail.com>
2020-06-25 15:20:43 -0700

    add Sign in page. Start signin and sign up components. Make form input."

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
+// Pages
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shoppage.component';
+import SignInandSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
+// Components
 import Header from './components/header/header.component';
 
 
@@ -14,6 +18,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInandSignUpPage} />
       </Switch>
     </div>
   );
