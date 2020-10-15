2d50466a0d73fb96e59e3225ab5737e1e7a278d2
Tyson Hood <TysonHood@gmail.com>
2020-10-15 11:26:49 -0700

    Set up the sign up component on the sign in page for email/password. Fixed styles.

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -32,7 +32,7 @@ class SignIn extends React.Component {
   render() {
     return (
       <div className='sign-in'>
-        <h2>I already have an account</h2>
+        <h2 className='title'>I already have an account</h2>
         <span>Sign in with your email and password.</span>
 
         <form onSubmit={this.handleSubmit}>
@@ -47,7 +47,7 @@ class SignIn extends React.Component {
             name='password' 
             type='password' 
             value={this.state.password} 
-            handleChange={this.handleChange}
+            handleChange={this.handleChange}  
             label='password'
             required 
           />
