2be06febd467f88cd3d062ab00b53ce5e05f63b6
Tyson Hood <TysonHood@gmail.com>
2020-10-31 15:32:42 -0700

    Further clean code. Add persist library to manage local storage for app.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
@@ -12,9 +13,11 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <App />
-
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:ital,wght@0,300;0,700;1,300&display=swap" rel="stylesheet"></link>
+        <PersistGate persistor={persistor}>
+          <App />
+          
+          <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:ital,wght@0,300;0,700;1,300&display=swap" rel="stylesheet"></link>
+        </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>,
